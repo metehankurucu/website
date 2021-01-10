d9544cb4b96275fa01c9e2edb40747e61240c876
Metehan Kurucu <43188732+metehankurucu@users.noreply.github.com>
2021-01-10 14:26:32 +0300

    add ga, sitemap.xml , robots.txt

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,11 @@
+require("dotenv").config()
+
 module.exports = {
   siteMetadata: {
     title: `Metehan Kurucu`,
     description: `Hi, I'm Metehan, this is my personal website.`,
     author: `@metehankurucu`,
+    siteUrl: `https://metehankurucu.com`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -41,6 +44,22 @@ module.exports = {
         start_url: `/`,
         display: `minimal-ui`,
         icon: `src/images/icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: process.env.GA_TRACKING_ID,
+      },
+    },
+    `gatsby-plugin-sitemap`,
+    {
+      resolve: "gatsby-plugin-robots-txt",
+      options: {
+        host: "https://metehankurucu.com",
+        sitemap: "https://metehankurucu.com/sitemap.xml",
+        policy: [{ userAgent: "*", allow: "/" }],
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
